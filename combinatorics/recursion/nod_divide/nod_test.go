651a10853c58cebd8bcae366acc522096b058151
trypophib1a <trypophib1a@mail.ru>
2022-01-15 03:00:37 +0500

    implementation nod

---
+++ combinatorics/recursion/nod_divide/nod_test.go
@@ -0,0 +1,32 @@
+package noddivide
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetNodeBySubtraction(t *testing.T) {
+	tests := []struct {
+		a        int
+		b        int
+		expected int
+	}{
+		{a: 4, b: 2, expected: 2},
+		{a: 32, b: 15, expected: 1},
+		{a: 16, b: 32, expected: 16},
+		{a: 12, b: 5, expected: 1},
+		{a: 3, b: 15, expected: 3},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(strconv.Itoa(tc.a), func(t *testing.T) {
+			subtraction := getNodBySubtraction(tc.a, tc.b)
+			mod := getNodByMod(tc.a, tc.b)
+			require.Equal(t, tc.expected, subtraction)
+			require.Equal(t, tc.expected, mod)
+		})
+	}
+}
